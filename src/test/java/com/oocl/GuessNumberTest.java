662ad9d59a3bd34ec388d43e4e91ab8502671693
CHANSHUNLEUNG <33828494+CHANSHUNLEUNG@users.noreply.github.com>
2020-04-08 18:21:05 +0800

    [leo] - add test case should_game_user_input_validate

--- src/test/java/com/oocl/GuessNumberTest.java
+++ src/test/java/com/oocl/GuessNumberTest.java
@@ -1,9 +1,22 @@
 package com.oocl;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class GuessNumberTest {
+    private GuessNumber game;
+    @Before
+    public void setUp() throws Exception {
+        game = new GuessNumber();
+    }
 
+    @Test
+    public void should_game_user_input_validate() {
+        assertEquals(true, game.validateUserInput("2 3 4 5"));
+        assertEquals(false, game.validateUserInput("a 3 4 5"));
+        assertEquals(false, game.validateUserInput("2 2 4 5"));
+        assertEquals(false, game.validateUserInput("2 2 4 5 6"));
+    }
 }
