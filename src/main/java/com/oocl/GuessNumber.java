6cdde50ba3937cf91080c4c2fd8a01b4472da371
CHANSHUNLEUNG <33828494+CHANSHUNLEUNG@users.noreply.github.com>
2020-04-08 18:18:04 +0800

    [leo] - validate user input

--- src/main/java/com/oocl/GuessNumber.java
+++ src/main/java/com/oocl/GuessNumber.java
@@ -1,6 +1,6 @@
 package com.oocl;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class GuessNumber {
     private static final int ANSWER_LENGTH = 4;
@@ -27,9 +27,36 @@ public class GuessNumber {
             }
         }
     }
+    public boolean validateUserInput(String userInputLine){
+        Set<String> userInputSet = new HashSet<>(Arrays.asList(userInputLine.split(" ")));
+        if(userInputLine.split(" ").length != 4){
+            return false;
+        }
+        if(userInputSet.size() != 4){
+            return false;
+        }
+        for(String userInputString :  userInputSet) {
+            try {
+                int userInputInteger = Integer.parseInt(userInputString);
+                if (userInputInteger < 0 || userInputInteger > 9) {
+                    return false;
+                }
+            } catch (Exception e) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main(String argv[]) {
         GuessNumber game = new GuessNumber();
         game.getAnswer().stream().forEach(System.out::println);
+        Scanner userInputScanner =  new Scanner(System.in);
+
+        while(userInputScanner.hasNext()){
+            String userInputLine = userInputScanner.nextLine();
+            System.out.println(game.validateUserInput(userInputLine));
+        }
+
     }
 }
--- src/test/java/com/oocl/GuessNumberTest.java
+++ src/test/java/com/oocl/GuessNumberTest.java
@@ -1,4 +1,9 @@
 package com.oocl;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class GuessNumberTest {
+
 }
